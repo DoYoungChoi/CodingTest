028a5ff12e8712a013562bdaf4a2a8058c93ec26
dodor <choidy9298@naver.com>
2023-11-10 13:49:01 +0900

    완전탐색 - 최소직사각형

--- CodingTest.playground/Pages/해시-베스트앨범.xcplaygroundpage/Contents.swift
+++ CodingTest.playground/Pages/해시-베스트앨범.xcplaygroundpage/Contents.swift
@@ -1,35 +1,17 @@
 import Foundation
 
-func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-    
-    var totalPlays: [String: Int] = [:]
-    var genreDict: [String: [[Int]]] = [:]
-    // classic: [[3, 800], [0, 500]], totalPlays: 1450
-    // pop: [[4, 2500], [1, 600]], totalPlays: 3100
-    
-    for (index, genre) in genres.enumerated() {
-        let playCount = plays[index]
-        if totalPlays[genre] != nil {
-            totalPlays[genre]! += playCount
-            var temp = genreDict[genre]!
-            temp.append([index, playCount])
-            temp.sort { $0[1] > $1[1] }
-            if temp.count > 2 {
-                temp.removeLast()
-            }
+let location = 0
+var priorities: [Int] = [1, 2, 9, 1, 2, 1]
 
-            genreDict[genre] = temp
-        } else {
-            totalPlays[genre] = playCount
-            genreDict[genre] = [[index, playCount]]
-        }
+var currentLocation = location
+var result = 1
+
+while (!priorities.isEmpty) {
+    if priorities.filter { $0 > priorities[0] }.isEmpty {
+        print("result: \(result)")
+    } else {
+        let value = priorities.removeFirst()
+        priorities.append(value)
+        result += 1
     }
-    
-    var result = [Int]()
-    totalPlays
-        .sorted { $0.value > $1.value }
-        .map { $0.key }
-        .forEach { genreDict[$0]!.forEach { result.append($0[0]) } }
-    
-    return result
 }
