1202e4ce768cd2f3a1b29359059085f7bb92bc22
dodor <choidy9298@naver.com>
2023-11-15 11:08:11 +0900

    Initial Commit

---
+++ CodingTest/CodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingTestApp.swift
+//  CodingTest
+//
+//  Created by dodor on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
