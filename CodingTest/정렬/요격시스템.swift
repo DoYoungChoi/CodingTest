a539d2a1a0b416c8fbca890117b10439d3c3ed0b
dodor <choidy9298@naver.com>
2024-01-07 14:15:32 +0900

    연습문제 - 요격 시스템

---
+++ CodingTest/정렬/요격시스템.swift
@@ -0,0 +1,23 @@
+//
+//  요격시스템.swift
+//  CodingTest
+//
+//  Created by dodor on 1/7/24.
+//
+
+import Foundation
+
+func solution(_ targets:[[Int]]) -> Int {
+    var targets = targets.sorted(by: { $0[1] < $1[1] })
+    var end = -1.0
+    var count = 0
+    
+    for target in targets {
+        if Double(target[0]) > end {
+            count += 1
+            end = Double(target[1]) - 0.1
+        }
+    }
+    
+    return count
+}
